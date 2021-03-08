6916639965145343a9f610e84ba1d4089def4ae6
oiorain <marion.bouguet@gmail.com>
2021-03-08 17:38:25 +0100

    add animation

--- main.js
+++ main.js
@@ -1,49 +1,9 @@
-// var updateUIInterval;
-// var isRunning = false;
-//
-// var seconds = 0;
-// var minutes = 0;
-
-// function updateUI() {
-//     seconds++;
-//
-//
-//     var secondString = '00';
-//     var minuteString = '00';
-//
-//     if (seconds === 60) {
-//         minutes++;
-//         seconds = 0;
-//     }
-    //
-    // secondString = (seconds < 10) ? `0${seconds}` : `${seconds}`
-    // minuteString = (minutes < 10) ? `0${minutes}` : `${minutes}`
-    //
-    // var timerString = `${minuteString}:${secondString}`;
-    // console.log(timerString);
-    // document.getElementById('timer').innerText = `${timerString}`;
-// }
-
-// document.getElementById('breathe').addEventListener('click', () => {
-//
-//     isRunning = !isRunning
-//
-//     if (isRunning) {
-//         // document.getElementById('breathe').innerText = "Stop"
-//         document.getElementById('bubble').style.animation = 'mover 10s infinite ease-in-out'
-//         updateUIInterval = setInterval(updateUI, 1000);
-//     } else {
-//         // document.getElementById('breathe').innerText = "Breathe"
-//         document.getElementById('bubble').style.animation = 'none'
-//         document.getElementById('timer').innerText = `00:00`;
-//
-//         seconds = 0;
-//         minutes = 0;
-//
-//         clearInterval(updateUIInterval);
-//     }
-// });
-
-// document.getElementById('breathe').innerText = "Stop"
-// document.getElementById('bubble').style.animation = 'mover 8s infinite ease-in-out'
-// updateUIInterval = setInterval(updateUI, 1000);
+document.getElementById('breathe').addEventListener('click', () => {
+  if (document.getElementById('breathe').innerText == "Bubble") {
+      document.getElementById('breathe').innerText = "Up-down"
+      document.getElementById('bubble').style.animationName = 'updown'
+  } else {
+    document.getElementById('breathe').innerText = "Bubble"
+    document.getElementById('bubble').style.animationName = 'bubble'
+  }
+});
