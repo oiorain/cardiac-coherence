75a1e6341ab15458ad3e4e360254e1f7c5b8650a
oiorain <marion.bouguet@gmail.com>
2021-03-08 17:45:00 +0100

    New Random color

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
 document.getElementById('breathe').addEventListener('click', () => {
   if (document.getElementById('breathe').innerText == "Bubble") {
       document.getElementById('breathe').innerText = "Up-down"
@@ -6,4 +15,8 @@ document.getElementById('breathe').addEventListener('click', () => {
     document.getElementById('breathe').innerText = "Bubble"
     document.getElementById('bubble').style.animationName = 'bubble'
   }
+});
+
+document.getElementById('color').addEventListener('click', () => {
+  document.getElementById('bubble').style.backgroundColor = getRandomColor()
 });
