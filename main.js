afd2fc6c439da934ae03863f58e3103af7b9c1f6
oiorain <marion.bouguet@gmail.com>
2021-03-27 17:38:23 +0100

    Add slower/faster

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
   var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
+  for (var i = 0; i < 6; i++)
+    color += '0123456789ABCDEF'[Math.floor(Math.random() * 16)];
   return color;
 }
+
+function displayPace(){
+  document.getElementById('time').innerText = pace+"secs"
+}
+function changePace(change){
+  pace += change;
+
+  document.getElementById('bubble').style.animationDuration = pace+"s";
+  displayPace();
+}
+
+let pace = 8;
+displayPace()
 
 document.getElementById('breathe').addEventListener('click', () => {
   if (document.getElementById('breathe').innerText == "Bubble") {
@@ -19,4 +30,13 @@ document.getElementById('breathe').addEventListener('click', () => {
 
 document.getElementById('color').addEventListener('click', () => {
   document.getElementById('bubble').style.backgroundColor = getRandomColor()
+});
+
+document.getElementById('faster').addEventListener('click', () => {
+  if (pace > 2)
+    changePace(-1);
+});
+
+document.getElementById('slower').addEventListener('click', () => {
+  changePace(+1)
 });
